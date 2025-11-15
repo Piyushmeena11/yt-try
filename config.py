ffc4e4cd7a4db58863d6e8893cd6ff0880d8d241
Piyush <meenapiyush23@gmail.com>
2025-11-15 22:22:45 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    API_ID = int(os.environ.get("API_ID", 0))
+    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    OWNER_ID = int(os.environ.get("OWNER_ID", 0))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", 0))
